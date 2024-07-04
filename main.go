612ea1e8ca6ebd279b56dd6da5a277cd8f1c5b6e
unknown <ashmitthawait2@gmail.com>
2024-07-05 03:31:29 +0530

    Restructured code, better backend handling

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 
 	"event_management/db"       // Import the db package
 	"event_management/handlers" // Import the handlers package
+)
+
+var (
+	templates = template.Must(template.ParseFiles(
+		"templates/index.html",
+	))
 )
 
 func main() {
@@ -13,11 +20,11 @@ func main() {
 	defer db.CloseDB()
 
 	// Serve static files
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// Serve index.html
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/index.html")
+		templates.ExecuteTemplate(w, "index.html", nil)
 	})
 
 	// Set up HTTP server and route handlers
