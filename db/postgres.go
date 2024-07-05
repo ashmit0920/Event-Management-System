fee0e0a56806ccc94cc8adb8079c35b7000e98fc
unknown <ashmitthawait2@gmail.com>
2024-07-05 23:47:31 +0530

    debugging docker compose and connection issues

--- db/postgres.go
+++ db/postgres.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
 	_ "github.com/lib/pq" // PostgreSQL driver
 )
@@ -13,15 +12,8 @@ var db *sql.DB
 
 // initialize the database connection
 func ConnectDB() {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-
-	connStr := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	//connStr := os.Getenv("DATABASE_URL")
+	connStr := "postgres://ashmit:test123@localhost:5432/event?sslmode=disable"
 
 	var err error
 	db, err = sql.Open("postgres", connStr)
